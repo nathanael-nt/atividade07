9e47c894c6a0c270cf55c9ccf4be0b7c2991f84e
nathanael-nt <nathanaelvieira17@gmail.com>
2024-11-01 21:35:26 -0300

    Add files via upload

---
+++ atidvidadess.py
@@ -0,0 +1,41 @@
+# ex1
+# import random
+
+# numeros = [0,1,2,3,4,5,6,7,8,9,10]
+# numero = random.choice(numeros)
+# escolha = 0
+# while numero != escolha:
+#     print('Tente adivinhar um número de 0 a 10!')
+#     escolha =  int(input('Digite um numero: '))
+
+# print('Acertou')
+    
+        
+# #ex2
+# usuario_real = "admin"
+# senha_correta = "1234"
+
+# while True:
+#     usuario = input("Digite o nome de usuário: ")
+#     senha = input("Digite a senha: ")
+
+#     if usuario == usuario_real and senha == senha_correta:
+#         print("Acesso concedido!")
+#         break
+#     else:
+#         print("Nome de usuário ou senha incorretos. Tente novamente.")
+        
+# #ex3
+# soma_total = 0
+
+# print("Digite números para somar. Para encerrar e ver o total, digite 0.")
+
+# while True:
+#     numero = float(input("Digite um número: "))
+    
+#     if numero == 0:
+#         break  
+    
+#     soma_total += numero 
+
+# print(f"A soma total é: {soma_total}")
